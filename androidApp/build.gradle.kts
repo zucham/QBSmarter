ae120e4e0d94a8ad56b83c7e843e66e2a889e31b
Matěj Žucha <zucham@noreply.codeberg.org>
2026-08-28 19:09:09 +0200

    Chore: Bump app version to v1.4.2

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         // API 31+. See AndroidManifest.xml for the declarations.
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdkVersion(rootProject.extra["defaultTargetSdkVersion"] as Int)
-        versionCode = 14
-        versionName = "1.4.1"
+        versionCode = 15
+        versionName = "1.4.2"
     }
     // -- Release signing --------------------------------------------------
     //
