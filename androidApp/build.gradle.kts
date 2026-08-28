699ef68e05c4717740234b7864644845afcb4efb
Matěj Žucha <zucham@noreply.codeberg.org>
2026-08-28 12:16:01 +0200

    Chore: Bump app version to v1.1.2

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         // API 31+. See AndroidManifest.xml for the declarations.
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdkVersion(rootProject.extra["defaultTargetSdkVersion"] as Int)
-        versionCode = 6
-        versionName = "1.1.1"
+        versionCode = 7
+        versionName = "1.1.2"
     }
     // -- Release signing --------------------------------------------------
     //
